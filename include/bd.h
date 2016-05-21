eea77a44c79851879a58745edd6889f8e16b9360
bigboss6 <bylnov.nick@google.com>
2016-05-21 14:05:50 +0300

    init bd

--- include/bd.h
+++ include/bd.h
@@ -14,54 +14,11 @@ private:
 private:
 	std::string fileName;
 public:
-
-	bd::bd() {
-		this->fileName = "AlertRequestHandler.txt";
-	}
-	bd::~bd() {}
-
-	std::vector<alert> bd::ReadDataBase() {
-		std::vector<alert> vec;
-		alert buf;
-		std::fstream file(this->fileName, std::ios::in);
-		if (!file.is_open()) {
-			file.close();
-			return vec;
-		}		
-		while (1)
-		{
-			file >> buf;                 //перегрузить fstream >> во всех гаджет
-			if (file.eof())
-				break;
-			vec.push_back(buf);
-		}
-		return vec;
-	}
-
-	int bd::WriteDataBase(std::vector<alert> &vec) {
-		std::vector<alert>::iterator beg = vec.begin();
-		std::vector<alert>::iterator end = vec.end();
-		std::fstream file(this->fileName, std::ios::out);
-		if (!file.is_open()) {
-			file.close();
-			return 0;
-		}
-		while (beg!=end) {
-			file << *beg;
-			beg++;
-		}
-		return 1;
-	}
-
-	int bd::put(alert newAlert) {
-		return 0;
-	}
-
-	int bd::get(alert newAlert) {
-		return 0;
-	}
-
-	int bd::remove(alert newAlert) {
-		return 0;
-	}
+	bd();
+	~bd() {}
+	std::vector<alert> bd::ReadDataBase();
+	int WriteDataBase(std::vector<alert>);
+	int put(alert);
+	int get(alert);
+	int remove(alert);	
 };
