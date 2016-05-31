145c486465cb74b10e62addb830508d8ae5618f4
German Volkov <Volkov.german.1997@gmail.com>
2016-05-31 19:54:44 +0300

    WebSite init commit.

--- old/client/alertSubSystem.h
+++
@@ -1,9 +0,0 @@
-#include "alert.h"
-
-namespace AlertSubSystem {
-    
-    std::vector<alert> get_alert(std::string origin);
-    int put_alert();
-    int sendToDaemon(std::string message);
-    
-}
